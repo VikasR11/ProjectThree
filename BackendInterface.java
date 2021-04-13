ed525fa342811ae555c7b107611257a7a5c035b4
Rohan Putcha <fastbiker703@gmail.com>
2021-04-12 22:11:56 -0400

    added getDistanceForAirportPath

--- BackendInterface.java
+++ BackendInterface.java
@@ -7,16 +7,18 @@
 // Lecturer: Gary Dahl
 // Notes to Grader: None
 
+import java.util.List;
 import java.util.NoSuchElementException;
 
 public interface BackendInterface {
-    AirportInterface[] getAirportShortestPath(String startAirportID, String endAirportID) throws NoSuchElementException;
+    List<AirportInterface> getAirportShortestPath(String startAirportID, String endAirportID) throws NoSuchElementException;
     boolean addAirport(AirportInterface airport);
-    boolean addFlight(String sourceID, String destinationID, double distance);
-    AirportInterface[] getReachableAirports(String origin) throws NoSuchElementException;
+    boolean addFlight(String sourceID, String destinationID, int distance);
+    List<AirportInterface> getReachableAirports(String origin) throws NoSuchElementException;
     boolean removeAirport(String airportID);
     boolean removeFlight(String origin, String destinationID);
     double getPriceForAirportPath(String startAirportID, String endAirportID) throws NoSuchElementException;
+    int getDistanceForAirportPath(String startAirportID, String endAirportID) throws NoSuchElementException;
     int getAirportCount();
     int getFlightCount();
 }
